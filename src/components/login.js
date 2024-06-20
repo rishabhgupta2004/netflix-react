9440fcee13034b6c528b77a3ae51522e73616e47
RISHABH GUPTA <rishabh78000gupta@gmail.com>
2024-06-20 12:25:08 +0530

    Update login.js

--- src/components/login.js
+++ src/components/login.js
@@ -1,9 +1,8 @@
 import React, { useRef, useState } from 'react';
 import Header from './header';
 import { checkValidData } from '../utils/validate';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { auth } from '../utils/firebase';
-import { updateProfile } from 'firebase/auth';
 import { addUser } from "../utils/userslice";
 import { useDispatch } from "react-redux";
 import { BG_URL, USER_AVATAR } from '../utils/constants';
@@ -61,24 +60,21 @@ const Login = () => {
   };
 
   return (
-    <div>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-cover bg-center" style={{ backgroundImage: `url(${BG_URL})` }}>
       <Header />
-      <div className="absolute">
-        <img src={BG_URL} alt="logos" />
-      </div>
-      <form onSubmit={(e) => e.preventDefault()} className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-50">
-        <h1 className="font-bold text-3xl py-4">{isSignForm ? 'Sign In' : 'Sign Up'}</h1>
+      <form onSubmit={(e) => e.preventDefault()} className="w-11/12 sm:w-8/12 md:w-6/12 lg:w-4/12 xl:w-3/12 p-8 bg-black text-white rounded-lg bg-opacity-70">
+        <h1 className="font-bold text-2xl sm:text-3xl py-4 text-center">{isSignForm ? 'Sign In' : 'Sign Up'}</h1>
         {!isSignForm && (
-          <input type="text" placeholder="Full Name" ref={name} className="p-4 my-4 w-full bg-gray-700" />
+          <input type="text" placeholder="Full Name" ref={name} className="p-4 my-4 w-full bg-gray-700 rounded" />
         )}
-        <input type="text" ref={email} placeholder="Email Address" className="p-4 my-4 w-full bg-gray-700" />
-        <input type="password" ref={password} placeholder="Password" className="p-4 my-4 w-full bg-gray-700" />
-        <p className="text-red-500 font-bold text-lg">{errorMessage}</p>
+        <input type="text" ref={email} placeholder="Email Address" className="p-4 my-4 w-full bg-gray-700 rounded" />
+        <input type="password" ref={password} placeholder="Password" className="p-4 my-4 w-full bg-gray-700 rounded" />
+        {errorMessage && <p className="text-red-500 font-bold text-lg text-center">{errorMessage}</p>}
         <button onClick={handleButtonClick} className="p-4 my-6 bg-red-700 rounded-lg w-full">
           {isSignForm ? 'Sign In' : 'Sign Up'}
         </button>
-        <p className="py-4" onClick={toggleSigninForm}>
-          {isSignForm ? 'New to Netflix? Sign Up Now' : 'Already registered'}
+        <p className="py-4 text-center cursor-pointer" onClick={toggleSigninForm}>
+          {isSignForm ? 'New to Netflix? Sign Up Now' : 'Already registered? Sign In'}
         </p>
       </form>
     </div>
